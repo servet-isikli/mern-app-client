8dcc12d00c13f82ba03cf7c5d152b12a638dd723
servet-isikli <servetisikli@yahoo.com>
2023-08-11 21:57:12 +0200

    forms

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Front-end</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,9 +1,171 @@
-import React from 'react'
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [userDetails, setUserDetails] = useState({
+    name: "",
+    email: "",
+    password: "",
+    checkPassword: "",
+  });
 
-export default Register
+  return (
+    <>
+      <link
+        href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        rel="stylesheet"
+        id="bootstrap-css"
+      />
+      <link
+        rel="stylesheet"
+        href="https://use.fontawesome.com/releases/v5.0.8/css/all.css"
+      />
+      <div className="container">
+        <br />
+        <div className="card bg-light">
+          <article className="card-body mx-auto" style={{ maxWidth: 400 }}>
+            <h4 className="card-title mt-3 text-center">Create Account</h4>
+            <p className="text-center">Get started with your free account</p>
+            <p>
+              <a href="/" className="btn btn-block btn-twitter">
+                {" "}
+                <i className="fab fa-twitter" /> &nbsp; Login via Twitter
+              </a>
+              <a href="/" className="btn btn-block btn-facebook">
+                {" "}
+                <i className="fab fa-facebook-f" /> &nbsp; Login via facebook
+              </a>
+            </p>
+            <p className="divider-text">
+              <span className="bg-light">OR</span>
+            </p>
+            <form>
+              <div className="form-group input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text">
+                    {" "}
+                    <i className="fa fa-user" />{" "}
+                  </span>
+                </div>
+                <input
+                  name="name"
+                  required
+                  value={userDetails.name}
+                  className="form-control"
+                  placeholder="Full name"
+                  type="text"
+                />
+              </div>{" "}
+              {/* form-group// */}
+              <div className="form-group input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text">
+                    {" "}
+                    <i className="fa fa-envelope" />{" "}
+                  </span>
+                </div>
+                <input
+                  name="email"
+                  required
+                  value={userDetails.email}
+                  className="form-control"
+                  placeholder="Email address"
+                  type="email"
+                />
+              </div>{" "}
+              {/* form-group// */}
+              <div className="form-group input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text">
+                    {" "}
+                    <i className="fa fa-phone" />{" "}
+                  </span>
+                </div>
+                <select className="custom-select" style={{ maxWidth: 120 }}>
+                  <option selected="">+971</option>
+                  <option value={1}>+972</option>
+                  <option value={2}>+198</option>
+                  <option value={3}>+701</option>
+                </select>
+                <input
+                  name=""
+                  className="form-control"
+                  placeholder="Phone number"
+                  type="text"
+                />
+              </div>{" "}
+              {/* form-group// */}
+              <div className="form-group input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text">
+                    {" "}
+                    <i className="fa fa-building" />{" "}
+                  </span>
+                </div>
+                <select className="form-control">
+                  <option selected=""> Select job type</option>
+                  <option>Designer</option>
+                  <option>Manager</option>
+                  <option>Accaunting</option>
+                </select>
+              </div>{" "}
+              {/* form-group end.// */}
+              <div className="form-group input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text">
+                    {" "}
+                    <i className="fa fa-lock" />{" "}
+                  </span>
+                </div>
+                <input
+                  name="password"
+                  required
+                  value={userDetails.password}
+                  className="form-control"
+                  placeholder="Create password"
+                  type="password"
+                />
+              </div>{" "}
+              {/* form-group// */}
+              <div className="form-group input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text">
+                    {" "}
+                    <i className="fa fa-lock" />{" "}
+                  </span>
+                </div>
+                <input
+                  name="checkPassword"
+                  required
+                  value={userDetails.checkPassword}
+                  className="form-control"
+                  placeholder="Repeat password"
+                  type="password"
+                />
+              </div>{" "}
+              {/* form-group// */}
+              <div className="form-group">
+                <button type="submit" className="btn btn-primary btn-block">
+                  {" "}
+                  Create Account
+                </button>
+              </div>{" "}
+              {/* form-group// */}
+              <p className="text-center">
+                Have an account? <Link to="/login">Log In</Link>{" "}
+              </p>
+            </form>
+          </article>
+        </div>{" "}
+        {/* card.// */}
+      </div>
+      {/*container end.//*/}
+      <br />
+      <br />
+      <article className="bg-secondary mb-3"></article>
+    </>
+  );
+};
+
+export default Register;
