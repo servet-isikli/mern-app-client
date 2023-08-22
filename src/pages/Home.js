aa51d4311270dc4ef53e423022e614f04315876a
servet-isikli <servetisikli@yahoo.com>
2023-08-22 18:50:18 +0200

    Cards edited

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,15 +2,32 @@ import React from "react";
 import ProductList from "../components/ProductList";
 import BCommerce from "../components/BCommerce";
 import { useProductListContext } from "../contexts/ProductListContext";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Home = () => {
   const products = useProductListContext();
 
   return (
     <div>
-      <h1>Home</h1>
-      <ProductList products={products} />
-      <BCommerce />
+      <Container>
+        <Row>
+          <Col sm={12} className="text-center">
+            <h1>Home</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={12}>
+            <ProductList products={products} />
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={12}>
+            <BCommerce />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
