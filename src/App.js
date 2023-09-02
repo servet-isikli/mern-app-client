167c031b2c58845a33b2c91ac7e3e76215a04851
servet-isikli <servetisikli@yahoo.com>
2023-09-03 01:02:12 +0200

    edit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import ProductDetailPage from "./pages/ProductDetailPage";
 import CreateProductPage from "./pages/CreateProductPage";
 import UpdateProductPage from "./pages/UpdateProductPage";
 import { Route, Routes } from "react-router-dom";
+import ProductGet from "./components/ProductGet";
+
+import ProductDetail from "./components/ProductDetail";
+import ProductCreate from "./components/ProductCreate";
+import ProductUpdate from "./components/ProductUpdate";
 import ProductDelete from "./components/ProductDelete";
 
 const App = () => {
@@ -14,6 +19,12 @@ const App = () => {
       <Route path="/update/:productId" element={<UpdateProductPage />} />
       <Route path="/detail/:productId" element={<ProductDetailPage />} />
       <Route path="/delete/:productId" element={<ProductDelete />} />
+
+      <Route path="/1" element={<ProductGet />} />
+      <Route path="/2/:productId" element={<ProductDetail />} />
+      <Route path="/3" element={<ProductCreate />} />
+      <Route path="/4/:productId" element={<ProductUpdate />} />
+      <Route path="/5/:productId" element={<ProductDelete />} />
     </Routes>
   );
 };
