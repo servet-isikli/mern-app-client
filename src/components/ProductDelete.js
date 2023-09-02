c507c40a3e048998266c8c0496788786717a3c58
servet-isikli <servetisikli@yahoo.com>
2023-09-03 00:42:24 +0200

    edit

--- src/components/ProductDelete.js
+++ src/components/ProductDelete.js
@@ -1,31 +1,25 @@
-import React, { useState, useEffect } from "react";
-import { useProductContext } from "../contexts/ProductContext";
-import { useParams, Link,  } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams, Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Alert from "react-bootstrap/Alert";
+import { deleteProduct, fetchProducts } from "../redux/actions/productActions"; // Redux eylemlerini ekleyin
 
 const ProductDelete = () => {
   const { productId } = useParams();
-  const { products, deleteProduct } = useProductContext();
-  const [product, setProduct] = useState(null);
-  const [isDeleted, setIsDeleted] = useState(false);
+  const dispatch = useDispatch();
+  const product = useSelector((state) =>
+    state.products.find((p) => p._id === productId)
+  );
+  const isDeleted = useSelector((state) => state.isDeleted);
 
   useEffect(() => {
-    const foundProduct = products.find((product) => product._id === productId);
-    if (foundProduct) {
-      setProduct(foundProduct);
-    }
-  }, [productId, products]);
+    dispatch(fetchProducts(productId)); // Ürün verilerini Redux store'dan yükleme
+  }, [dispatch, productId]);
 
   const handleDelete = () => {
-    deleteProduct(productId)
-      .then(() => {
-        setIsDeleted(true);
-      })
-      .catch((error) => {
-        console.error("Error deleting product:", error);
-      });
+    dispatch(deleteProduct(productId));
   };
 
   return (
@@ -38,7 +32,12 @@ const ProductDelete = () => {
             <Card.Text>{product.description}</Card.Text>
             <Card.Text>Price: ${product.price}</Card.Text>
             {isDeleted ? (
-              <Alert variant="success">Product deleted successfully</Alert>
+              <div>
+                <Alert variant="success">Product deleted successfully</Alert>
+                <Link to={`/products`}>
+                  <Button variant="primary">Back to Products</Button>
+                </Link>
+              </div>
             ) : (
               <div>
                 <Button variant="danger" onClick={handleDelete}>
@@ -52,7 +51,7 @@ const ProductDelete = () => {
           </Card.Body>
         </Card>
       ) : (
-        <p>Loading...</p>
+        <p>Product not found</p>
       )}
     </div>
   );
