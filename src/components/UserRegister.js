84dbdc5f08ca91c5bc182c03c15c5c09b25b19dc
servet-isikli <servetisikli@yahoo.com>
2023-09-09 14:59:28 +0200

    registerPage is ready

--- src/components/UserRegister.js
+++ src/components/UserRegister.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
+import { registerUser } from "../redux/actions/userActions";
 
 const UserRegister = () => {
   const dispatch = useDispatch();
-  const registerUser = useSelector((state) => state.users.registerUser);
+  const registeredUser = useSelector((state) => state.users.registeredUser);
 
   const initialFormData = {
     username: "",
@@ -26,7 +28,41 @@ const UserRegister = () => {
     dispatch(registerUser(formData));
   };
 
-  return <div>UserRegister</div>;
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="username">
+        <Form.Label>Username</Form.Label>
+        <Form.Control
+          type="text"
+          name="username"
+          value={formData.username}
+          onChange={handleInputChange}
+        />
+      </Form.Group>
+      <Form.Group controlId="email">
+        <Form.Label>E-Mail</Form.Label>
+        <Form.Control
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleInputChange}
+        />
+      </Form.Group>
+      <Form.Group controlId="password">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleInputChange}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Create User
+      </Button>
+      {registeredUser && <p>User created with ID: {registeredUser._id}</p>}
+    </Form>
+  );
 };
 
 export default UserRegister;
