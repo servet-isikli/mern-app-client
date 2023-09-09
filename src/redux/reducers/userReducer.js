1552bbe0f90c51dc37988b3b3cb4249c4e96546c
servet-isikli <servetisikli@yahoo.com>
2023-09-09 14:21:56 +0200

    userReducer

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,14 +1,56 @@
-// src/reducers/userReducer.js
+// userReducer.js dosyası
+
+import {
+  LOGIN_USER_REQUEST,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAILURE,
+  LOGOUT_USER_REQUEST,
+  LOGOUT_USER_SUCCESS,
+  LOGOUT_USER_FAILURE,
+  REGISTER_USER_REQUEST,
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_FAILURE,
+} from "../actions/actionTypes";
+
 const initialState = {
-  user: null,
+  username: "",
+  email: "",
+  password: "",
+  isLoggedIn: false,
+  isLoading: false,
+  error: null,
 };
 
 const userReducer = (state = initialState, action) => {
   switch (action.type) {
-    case "SET_USER":
-      return { ...state, user: action.payload };
-    case "CLEAR_USER":
-      return { ...state, user: null };
+    case REGISTER_USER_REQUEST:
+    case LOGIN_USER_REQUEST:
+    case LOGOUT_USER_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case REGISTER_USER_SUCCESS:
+    case LOGIN_USER_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        isLoggedIn: true,
+      };
+    case REGISTER_USER_FAILURE:
+    case LOGIN_USER_FAILURE:
+    case LOGOUT_USER_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload.error,
+      };
+    case LOGOUT_USER_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        isLoggedIn: false,
+      };
     default:
       return state;
   }
