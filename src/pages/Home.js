c28729afe054318c366aa65f9c049294d086c421
servet-isikli <servetisikli@yahoo.com>
2023-09-24 18:26:54 +0200

    CardsBlog

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,19 +4,26 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Carousels from "../components/Carousels";
+import CardsBlog from "../components/CardsBlog";
+
 
 const Home = () => {
   return (
     <div>
       <Container>
         <Row>
-          <Col sm={12} className="text-center">
-          </Col>
+          <Col sm={12} className="text-center"></Col>
         </Row>
 
         <Row>
           <Col sm={12} className="text-center">
             <Carousels />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col sm={12} className="">
+            <CardsBlog />
           </Col>
         </Row>
 
